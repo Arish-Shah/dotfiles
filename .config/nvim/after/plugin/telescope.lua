861f73b349a4857a24ea4b09f12dc2c8eeabd007
Arish-Shah <arish.r.shah@gmail.com>
2023-04-29 17:46:29 +0200

    fix: telescope plugin

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,8 +1,22 @@
+local function project_files()
+  local opts = {}
+  vim.fn.system("git rev-parse --is-inside-work-tree")
+  if vim.v.shell_error == 0 then
+    require("telescope.builtin").git_files(opts)
+  else
+    require("telescope.builtin").find_files(opts)
+  end
+end
+
 require("telescope").setup({
   defaults = {
     file_ignore_patterns = { "node_modules" },
   },
+  pickers = {
+    find_files = {
+      find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+    },
+  },
 })
 
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+vim.keymap.set("n", "<C-p>", project_files, {})

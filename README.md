6598f92b4eb71691063bb7eedefe631baefadeca
Arish-Shah <arish.r.shah@gmail.com>
2023-04-30 13:39:44 +0200

    update: script

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # dotfiles
+
+wsl setup
+
+```{bash}
+curl -o- https://raw.githubusercontent.com/Arish-Shah/dotfiles/main/install.sh | bash
+```
--- install.sh
+++ install.sh
@@ -1,55 +1,49 @@
 #!/usr/bin/env bash
 
-echo -e "removing vim...\n"
+# remove vim, update system, install dependencies
 sudo apt autoremove --purge vim -y
-
-echo -e "updating system...\n"
 sudo apt update && sudo apt upgrade -y
-
-echo -e "installing dependencies...\n"
 sudo apt install fzf exa zsh build-essential gh ripgrep libfuse2 --no-install-suggests --no-install-recommends -y
 
-echo -e "making directories...\n"
-mkdir -p ./{.local/{share,state},.cache/{zsh},.config,dev}
+# make directories
+mkdir -p ./{.local/{share,state/zsh},.cache/zsh,.config,dev}
 
-echo -e "cloning dotfiles...\n"
-cd dev
-git clone https://github.com/Arish-Shah/dotfiles --depth=1
-cd
+# clone dotfiles
+git clone https://github.com/Arish-Shah/dotfiles --depth=1 ~/dev/dotfiles
 
-echo -e "installing oh-my-zsh...\n"
+# install oh-my-zsh
 export ZSH="$HOME/.local/share/oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-echo -e "copying dotfiles...\n"
+# copy all my dotfiles
 cp ~/dev/dotfiles/.zsh* ~
 cp ~/dev/dotfiles/.tmux.conf ~
 cp ~/dev/dotfiles/.config/npm ~/.config -r
 cp ~/dev/dotfiles/.config/nvim ~/.config -r
 cp ~/dev/dotfiles/.local/bin ~/.local -r
 
-echo -e "configuring git...\n"
+# configure git
 git config --global init.defaultBranch main
 git config --global user.name "Arish-Shah"
 git config --global user.email "arish.r.shah@gmail.com"
 
-echo -e "installing node version manager...\n"
+# install node version manager
 export NVM_DIR="$HOME/.local/share/nvm"
 mkdir $NVM_DIR
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
-echo -e "downloading neovim...\n"
+# download neovim
 wget -P ~/.local/bin/ https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
 chmod +x ~/.local/bin/nvim.appimage
 
-echo -e "starting zsh and installing nvm...\n"
+# install node.js using nvm
 source ~/.bashrc
 nvm install --lts
 
-echo -e "changing user shell to zsh...\n"
+# change user shell to zsh
 sudo chsh -s /usr/bin/zsh $USER
 
-echo -e "removing files...\n"
+# remove garbago files
 rm .bash* .motd_shown .profile .sudo_as_admin_successful .wget-hsts
 
 # login to gh

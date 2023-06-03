00a96844a22ff90493475500b095d321b64d68f1
Arish-Shah <arish.r.shah@gmail.com>
2023-06-03 02:37:25 -0700

    move to on_attach

--- .config/nvim/after/plugin/tree.lua
+++ .config/nvim/after/plugin/tree.lua
@@ -1,32 +1,36 @@
-require("nvim-tree").setup()
+local function on_attach()
+  local api = require("nvim-tree.api")
 
-local api = require("nvim-tree.api")
+  local function edit_or_open()
+    local node = api.tree.get_node_under_cursor()
 
-local function edit_or_open()
-  local node = api.tree.get_node_under_cursor()
-
-  if node.nodes ~= nil then
-    api.node.open.edit()
-  else
-    api.node.open.edit()
-    api.tree.close()
+    if node.nodes ~= nil then
+      api.node.open.edit()
+    else
+      api.node.open.edit()
+      api.tree.close()
+    end
   end
+
+  local function vsplit_preview()
+    local node = api.tree.get_node_under_cursor()
+
+    if node.nodes ~= nil then
+      api.node.open.edit()
+    else
+      api.node.open.vertical()
+    end
+    api.tree.focus()
+  end
+
+  vim.keymap.set("n", "l", edit_or_open)
+  vim.keymap.set("n", "L", vsplit_preview)
+  vim.keymap.set("n", "h", api.tree.close)
+  vim.keymap.set("n", "H", api.tree.collapse_all)
 end
 
-local function vsplit_preview()
-  local node = api.tree.get_node_under_cursor()
-
-  if node.nodes ~= nil then
-    api.node.open.edit()
-  else
-    api.node.open.vertical()
-  end
-  api.tree.focus()
-end
+require("nvim-tree").setup({
+  on_attach = on_attach,
+})
 
 vim.api.nvim_set_keymap("n", "<C-h>", ":NvimTreeToggle<cr>", { silent = true, noremap = true })
-
-vim.keymap.set("n", "l", edit_or_open)
-vim.keymap.set("n", "L", vsplit_preview)
-vim.keymap.set("n", "h", api.tree.close)
-vim.keymap.set("n", "H", api.tree.collapse_all)

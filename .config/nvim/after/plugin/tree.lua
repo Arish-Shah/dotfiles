783d0f41510244467d268ca48479ebbf9ba84243
Arish-Shah <arish.r.shah@gmail.com>
2023-07-26 20:51:19 +0200

    update nvim-tree and auto-focus

--- .config/nvim/after/plugin/tree.lua
+++ .config/nvim/after/plugin/tree.lua
@@ -52,7 +52,7 @@ local function on_attach(bufnr)
 
   vim.keymap.set("n", "l", edit_or_open, opts("Edit Or Open"))
   vim.keymap.set("n", "L", vsplit_preview, opts("Vsplit Preview"))
-  vim.keymap.set("n", "h", api.tree.close, opts("Close"))
+  vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close Directory"))
   vim.keymap.set("n", "H", api.tree.collapse_all, opts("Collapse All"))
 end
 

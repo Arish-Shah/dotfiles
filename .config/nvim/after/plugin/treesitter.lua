a0236d8be849e46737a1bb0d5096e2d649ba37f9
Arish-Shah <arish.r.shah@gmail.com>
2024-02-13 03:20:07 +0100

    fix: options

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,3 +1,6 @@
 require("nvim-treesitter.configs").setup({
-  ensure_installed = { "lua" },
+  ensure_installed = { "lua", "python", "typescript" },
+  sync_install = false,
+  highlight = { enable = true },
+  indent = { enable = true },
 })
